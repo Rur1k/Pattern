d327c4f0580dbfc9299bbce0317f9989b467cb37
Nikita Tishchenko <falgerok.master@gmail.com>
2021-04-26 17:05:44 +0300

    Реализован Singleton

--- Singlenton.py
+++ Singlenton.py
@@ -1,10 +1,13 @@
-class Singleton(object):
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Singleton, cls).__new__(cls)
-            return cls.instance
+class Singleton:
+    __instance = None
 
-s = Singleton()
-print("Созданный object:", s)
-s1 = Singleton()
-print("Созданный object:", s1)
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is not None:
+            return cls.__instance
+        cls.__instance = super(Singleton, cls).__new__(cls)
+        return cls.__instance
+
+evil1 = Singleton()
+evil2 = Singleton()
+
+print(evil1, evil2)
