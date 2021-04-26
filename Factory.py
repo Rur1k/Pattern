6e29e54e409bc2e1c5dbd0cc60f050068095b5d6
Nikita Tishchenko <falgerok.master@gmail.com>
2021-04-26 15:12:54 +0300

    Реализован фабричный метод

--- Factory.py
+++ Factory.py
@@ -1,19 +1,36 @@
 class Transport:
     def CreateTransport(self):
-        pass
+        raise NotImplementedError()
 
 class AirTransport(Transport):
     def CreateTransport(self):
-        pass
+        return 'Create air transport'
 
 class SeaTransport(Transport):
     def CreateTransport(self):
-        pass
+        return 'Create sea transport'
 
 class LandTransport(Transport):
     def CreateTransport(self):
-        pass
+        return 'Create land transport'
 
-Transport = Transport()
+class Creator:
+    def create_transport(self, type_): #фабричный метод с парамметрами.
+        raise NotImplementedError()
 
-Car = Transport.CreateTransport()
+class MyCreator(Creator):
+    def create_transport(self, type_):
+        if type_ == 'Air':
+            return AirTransport()
+        elif type_ == 'Sea':
+            return SeaTransport()
+        elif type_ == 'Land':
+            return LandTransport()
+        else:
+            raise ValueError()
+
+CreatorTransport = MyCreator()
+
+print(CreatorTransport.create_transport('Air').CreateTransport())
+print(CreatorTransport.create_transport('Sea').CreateTransport())
+print(CreatorTransport.create_transport('Land').CreateTransport())
